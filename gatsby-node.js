77126d09f6c78c2e00a8b3910e36503ecd4442d5
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-02 11:51:38 -0800

    Add ability to configure output destination (#1)
    * Add ability to configure output destination
    
    * Update gatsby-node.js
    
    Co-Authored-By: damassi <damassi.pappas@gmail.com>
    
    * Update gatsby-node.js
    
    Co-Authored-By: damassi <damassi.pappas@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,22 @@
-const fs = require("fs");
+const write = require("write");
 const path = require("path");
 const { introspectionQuery, graphql } = require("gatsby/graphql");
 
-const snapshotLocation = path.resolve(process.cwd(), "schema.json");
+const defaultLocation = path.resolve(process.cwd(), "schema.json");
 
-exports.onPostBootstrap = ({ store }) =>
+exports.onPostBootstrap = ({ store }, options) => {
+  const dest = options.dest || defaultLocation;
   new Promise((resolve, reject) => {
     const { schema } = store.getState();
     graphql(schema, introspectionQuery)
-      .then(res => fs.writeFileSync(snapshotLocation, JSON.stringify(res.data)))
+      .then(res => write.sync(dest, JSON.stringify(res.data)))
       .then(() => {
-        console.log("Wrote schema");
+        console.log("[gatsby-plugin-extract-schema] Wrote schema");
         resolve();
       })
       .catch(e => {
-        console.log("Failed to write schema: ", e);
+        console.error("[gatsby-plugin-extract-schema] Failed to write schema: ", e);
         reject();
       });
   });
+}
