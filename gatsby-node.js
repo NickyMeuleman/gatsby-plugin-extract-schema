b7ad506f20bad21a09db2689ef23c680ccc96c06
a-rts <a-rts@users.noreply.github.com>
2019-05-30 00:37:20 +0100

    Added option to nest schema under data property

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,25 @@
 const write = require("write");
 const path = require("path");
-const { introspectionQuery, graphql } = require("gatsby/graphql");
+const {introspectionQuery, graphql} = require("gatsby/graphql");
 
 const defaultLocation = path.resolve(process.cwd(), "schema.json");
 
-exports.onPostBootstrap = ({ store }, options) => {
-  const dest = options.dest || defaultLocation;
-  new Promise((resolve, reject) => {
-    const { schema } = store.getState();
-    graphql(schema, introspectionQuery)
-      .then(res => write.sync(dest, JSON.stringify(res.data)))
-      .then(() => {
-        console.log("[gatsby-plugin-extract-schema] Wrote schema");
-        resolve();
-      })
-      .catch(e => {
-        console.error("[gatsby-plugin-extract-schema] Failed to write schema: ", e);
-        reject();
-      });
-  });
-}
+exports.onPostBootstrap = ({store}, options) => {
+    const dest = options.dest || defaultLocation;
+    new Promise((resolve, reject) => {
+        const {schema} = store.getState();
+        graphql(schema, introspectionQuery)
+            .then(res => {
+                const content = options.nested ? {data: res.data} : res.data;
+                write.sync(dest, JSON.stringify(content));
+            })
+            .then(() => {
+                console.log("[gatsby-plugin-extract-schema] Wrote schema");
+                resolve();
+            })
+            .catch(e => {
+                console.error("[gatsby-plugin-extract-schema] Failed to write schema: ", e);
+                reject();
+            });
+    });
+};
